dafeaf5dc7be4dcc70aacef60de75f46e2d4df87
Aditya Vyavahare <vvaditya23@icloud.com>
2022-08-09 12:05:59 +0530

    Implemented MVC design pattern.

--- Xylophone/Controller/ViewController.swift
+++ Xylophone/Controller/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         //soundName = sender.title(for: .normal)! //OR
         //soundName = sender.currentTitle!
         
+    //Fades out the button on click
         sender.alpha = 0.5
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
               //Bring's sender's opacity back up to fully opaque.
@@ -40,7 +41,6 @@ class ViewController: UIViewController {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
     }
 }
 
